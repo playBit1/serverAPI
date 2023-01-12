948820d9bb9f609b67453c53f1eb479eef0865ea
playBit1 <87233951+playBit1@users.noreply.github.com>
2023-01-13 03:08:44 +0530

    Add files via upload

---
+++ rest_api_app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, request
+import ml_model_loader as ml
+
+app = Flask(__name__)
+
+
+# POST method to collect user data and return model output
+@app.route('/model', methods=['POST'])
+def post_model():
+    data = request.get_json()
+    data_name = data['virusName']
+    data_list = data['userData']
+
+    #model = ml.MlModelLoader(data_name, data_list)
+    #final_output = model.run_model()
+
+    return data_list
+
+
+if __name__ == '__main__':
+    app.run()
